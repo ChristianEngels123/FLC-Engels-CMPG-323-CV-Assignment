7a881cc09afeec14d8b11d1c567f864ef3d3a590
ChristianEngels123 <christian.flce@gmail.com>
2025-07-25 16:13:36 +0200

    Update
    Added elements to Projects, but will change.

---
+++ Projects/load_projects.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+	const container = document.getElementById("proj-1");
+	if (typeof proj1Text !== "undefined" && container) {
+		container.innerHTML = proj1Text;
+	} else {
+		container.innerText = "Failed to load About Me content.";
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+	const container = document.getElementById("proj-2");
+	if (typeof proj1Text !== "undefined" && container) {
+		container.innerHTML = proj1Text;
+	} else {
+		container.innerText = "Failed to load About Me content.";
+  }
+});
---
+++ Projects/proj_1.js
@@ -0,0 +1,3 @@
+const proj1Text = `<h2>Eagle Eye Security: CMPG 213 & 223 Project</h2>
+<p>This project was an assingment in my 2nd year of B.Sc. Information Technology</p>
+`;
