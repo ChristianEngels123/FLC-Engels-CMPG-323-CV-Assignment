547a6b30b97faecfa00cfb509d227ffa6cd7ffc8
ChristianEngels123 <christian.flce@gmail.com>
2025-07-25 07:34:32 +0200

    Update
    Added work experience to Home Page.

--- about/about_me.js
+++ about/about_me.js
@@ -12,4 +12,8 @@ Feel free to explore the site:
   <li><strong>Certificates</strong> - Highlights qualifications I've earned during my studies</li>
 </ul>
 <p>Thank you for visiting!</p>
+
+<h2>Work Experience</h2>
+<p>In 2022 I ganined work experience through working as a <strong>Bartender</strong> at Rocking Potch for the duration of the Festival.</p>
+<p>And in 2024 I gained further work experience and insight into <strong>Industry</strong> through shadowing the <strong>IT Department</strong> of KLK Landbou Ltd. for a week. During this time I assisted the company by helping to program routers under the supervision of their Main Programmer. I also lead a <strong>Research Project</strong> for the company, looking into the use of <strong>NFC Technology</strong> to create Membership Cards for Customers that have accounts at the company that would work through a Point-of-Sale System.</p>
 `;
