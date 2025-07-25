55bffc7c1f09364b7ca89bdeb4e7b34396ec6bd7
ChristianEngels123 <christian.flce@gmail.com>
2025-07-25 18:16:18 +0200

    Update
    Added both projects that will be displayed on the website

--- Projects/proj_1.js
+++ Projects/proj_1.js
@@ -1,3 +1,19 @@
-const proj1Text = `<h2>Eagle Eye Security: CMPG 213 & 223 Project</h2>
-<p>This project was an assingment in my 2nd year of B.Sc. Information Technology</p>
-`;
+const proj1Text = `In the fictional company that was created the current system for keeping track of personnel, equipment, shifts, etc., was outdated as they were still using pen, paper, ledgers and almanacs isntead of a modern digitized sytem. The problem that was presented to us was the following:
+					<ul>
+						<li>A digitized system that can automate the following processes:</li>
+						<ul>
+							<li>Assigning security personnel to their shifts</li>
+							<li>Inventory control over assigned equipment and vehicles</li>
+							<li>Salary calculations</li>
+							<li>Sector updates according to after-shift reports</li>
+						</ul>
+						<li>A better reporting structure is required that can provide thoroughly detailed reports on:</li>
+						<ul>
+							<li>Sector danger levels</li>
+							<li>Equipment and vehicles that are currently assigned to security personnel</li>
+							<li>Monthly salaries to be paid</li>
+							<li>Changes in sector danger levels</li>
+						</ul>
+						<li>The current system does not keep track of engagements in the field, i.e., when personnel are engaged on a call out</li>
+						<li>A help function will not be implemented as the company operates in a high stress environment and all personnel employed have to go through various forms of training beforehand, thus temporary employees will not be hired</li>
+					</ul>`;
